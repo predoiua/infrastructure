9391e5b20f6cf9116abc7381f641b58e4edca78b
AP <predoiu.seli@gmail.com>
2019-11-19 20:20:50 +0200

    wip

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -8,7 +8,7 @@ sudo yum update -y
 sudo yum install -y epel-release
 sudo yum update -y
 #sudo yum groupinstall -y "Server with GUI"
-sudo yum group install -y "MATE Desktop"
+sudo yum group install -y "X Window system" "MATE Desktop"
 # "Graphical Administration Tools"
 
 #install chrome
