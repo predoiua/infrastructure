be0be5fd48fe9654238a5b226b41ea90749d99f3
AP <predoiua@vv10.eu>
2023-08-12 17:50:07 +0300

    remove obsolete ansible roles

--- ansible/provision/provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-HOST=hosts/local.ini
-PLAYBOOK=provision.yaml
-#ansible-playbook -i ${HOST} ${PLAYBOOK} -e @${CONFIG} -e ansible_ssh_user=ansible
-ansible-playbook -i ${HOST} ${PLAYBOOK} -e ansible_ssh_user=ansible
